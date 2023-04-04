89cc5c21d0b3a139b6c4bcf3286a931e6dd8d0fb
Jimmy Fitzback <>
2023-04-04 23:25:18 +0900

    feat: 🎸 node to CLIP Encode prompt & negative prompt at once

---
+++ custom_nodes/conditioning_CLIP_RawText_Encode.py
@@ -0,0 +1,22 @@
+class CLIPRawTextEncode:
+
+    def __init__(self):
+        pass
+    @classmethod
+    def INPUT_TYPES(s):
+        return {"required": {"prompt": ("RAW_TEXT", {"multiline": True}),"neg_prompt": ("RAW_TEXT", {"multiline": True}), "clip": ("CLIP", )}}
+    RETURN_TYPES = ("CONDITIONING","CONDITIONING",)
+    RETURN_NAMES = ("positive", "negative")
+    FUNCTION = "encode"
+
+    CATEGORY = "conditioning"
+
+    def encode(self, clip, prompt,neg_prompt):
+        return (
+            [[clip.encode(str(prompt)), {}]],
+            [[clip.encode(str(neg_prompt)), {}]]
+        , )
+    
+NODE_CLASS_MAPPINGS = {
+    "CLIP_RawTextEncode_ToPrompts": CLIPRawTextEncode,
+}
