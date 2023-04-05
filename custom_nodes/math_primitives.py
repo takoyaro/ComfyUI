6d90e9d65f72d9ac7565a6afe43a6c307d2d8c39
Jimmy Fitzback <>
2023-04-05 22:11:11 +0900

    feat: 🎸 math primitives

---
+++ custom_nodes/math_primitives.py
@@ -0,0 +1,42 @@
+class FloatInput:
+    def __init__(self):
+        pass
+    
+    @classmethod
+    def INPUT_TYPES(s):
+        return {"required": 
+                    {"_float": ("FLOAT", {"default": 0.0, "step": 0.01})},
+        }
+
+    RETURN_TYPES = ("FLOAT",)
+    FUNCTION = "exec"
+
+    CATEGORY = "Math"
+
+    def exec(self, _float=0.0):
+        print(f"FloatInput: {_float}")
+        return { _float }
+
+class IntInput:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(s):
+        return {"required": 
+                    {"_int": ("INT", {"default": 0, "step": 1})},
+        }
+
+    RETURN_TYPES = ("INT",)
+    FUNCTION = "exec"
+
+    CATEGORY = "Math"
+
+    def exec(self, _int=0):
+        print(f"IntInput: {_int}")
+        return { _int }
+
+NODE_CLASS_MAPPINGS = {
+    "FloatInput": FloatInput,
+    "IntInput": IntInput,
+}
