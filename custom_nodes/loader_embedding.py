29e151d5fb0aa5a6ea01cf3fec2002e76b234cb9
Jimmy Fitzback <>
2023-04-05 20:04:08 +0900

    feat: 🎸 added embedding strength

--- custom_nodes/loader_embedding.py
+++ custom_nodes/loader_embedding.py
@@ -7,16 +7,21 @@ class EmbeddingLoader:
 
     @classmethod
     def INPUT_TYPES(s):
-        return {"required": { "embedding_name": (folder_paths.get_filename_list("embeddings"), )}}
+        return {
+            "required": { 
+                "embedding_name": (folder_paths.get_filename_list("embeddings"), ),
+                "strength": ("FLOAT", {"min": 0, "max": 64, "default": 1, "step": 0.1})
+            }
+        }
 
     RETURN_TYPES = ("RAW_TEXT",)
     FUNCTION = "load_embedding"
 
     CATEGORY = "loaders"
 
-    def load_embedding(self, embedding_name):
+    def load_embedding(self, embedding_name,strength):
 
-        return (f'embedding:{embedding_name}',)
+        return (f'embedding:{embedding_name}:{strength}',)
 
 NODE_CLASS_MAPPINGS = {
     "EmbeddingLoader": EmbeddingLoader,
